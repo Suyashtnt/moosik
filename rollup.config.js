e24fd22221a85c034021625435cf405c399a9c20
Restyled.io <commits@restyled.io>
2020-09-08 10:03:15 +0000

    Restyled by clang-format

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import typescript from '@rollup/plugin-typescript';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
@@ -5,146 +6,106 @@ import resolve from 'rollup-plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
 import replace from 'rollup-plugin-replace';
 import svelte from 'rollup-plugin-svelte';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import {terser} from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
-import pkg from './package.json';
 import sveltePreprocess from 'svelte-preprocess';
+
+import pkg from './package.json';
+
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === 'CIRCULAR_DEPENDENCY' &&
-    /[/\\]@sapper[/\\]/.test(warning.message)) ||
-  onwarn(warning);
+    (warning.code === 'CIRCULAR_DEPENDENCY' &&
+     /[/\\]@sapper[/\\]/.test(warning.message)) ||
+    onwarn(warning);
 
 const dedupe = importee =>
-  importee === 'svelte' || importee.startsWith('svelte/');
+    importee === 'svelte' || importee.startsWith('svelte/');
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.html', './src/**/*.svelte', './src/**/*.css'],
+  content : [ './src/**/*.html', './src/**/*.svelte', './src/**/*.css' ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor : content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 });
 
 const preprocess = sveltePreprocess({
-  postcss: {
-    plugins: [require('postcss-import')(), require('postcss-nested')()]
-  }
+  postcss :
+      {plugins : [ require('postcss-import')(), require('postcss-nested')() ]}
 });
 
 export default {
   client: {
     input: config.client.input(),
     output: config.client.output(),
-    plugins: [
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode)
-      }),
-      svelte({
-        preprocess,
-        dev,
-        hydratable: true,
-        emitCss: true
-      }),
-      resolve({
-        browser: true,
-        dedupe,
-        mainFields: ['main']
-      }),
-      commonjs(),
-      json(),
-      typescript(),
-      legacy &&
-        babel({
-          extensions: ['.js', '.mjs', '.html', '.svelte'],
-          runtimeHelpers: true,
-          exclude: ['node_modules/@babel/**'],
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: '> 0.25%, not dead'
-              }
+    plugins:
+        [
+          replace({
+            'process.browser' : true,
+            'process.env.NODE_ENV' : JSON.stringify(mode)
+          }),
+          svelte({preprocess, dev, hydratable : true, emitCss : true}),
+          resolve({browser : true, dedupe, mainFields : [ 'main' ]}),
+          commonjs(), json(), typescript(),
+          legacy && babel({
+            extensions : [ '.js', '.mjs', '.html', '.svelte' ],
+            runtimeHelpers : true,
+            exclude : [ 'node_modules/@babel/**' ],
+            presets :
+                [ [ '@babel/preset-env', {targets : '> 0.25%, not dead'} ] ],
+            plugins : [
+              '@babel/plugin-syntax-dynamic-import',
+              [ '@babel/plugin-transform-runtime', {useESModules : true} ]
             ]
-          ],
-          plugins: [
-            '@babel/plugin-syntax-dynamic-import',
-            [
-              '@babel/plugin-transform-runtime',
-              {
-                useESModules: true
-              }
-            ]
-          ]
-        }),
+          }),
 
-      !dev &&
-        terser({
-          module: true
-        })
-    ],
+          !dev && terser({module : true})
+        ],
     onwarn
   },
 
   server: {
     input: config.server.input(),
     output: config.server.output(),
-    plugins: [
-      replace({
-        'process.browser': false,
-        'process.env.NODE_ENV': JSON.stringify(mode)
-      }),
-      svelte({
-        preprocess,
-        generate: 'ssr',
-        dev
-      }),
-      postcss({
-        extract: './static/global.css',
-        plugins: [
-          require('postcss-import'),
-          require('tailwindcss'), // See tailwind.config.js
-          require('autoprefixer'),
-          require('postcss-fail-on-warn'),
-          // Do not purge the CSS in dev mode to be able to play with classes in the browser dev-tools.
-          !dev && purgecss,
-          !dev &&
-            require('cssnano')({
-              preset: 'default'
-            })
-        ].filter(Boolean)
-      }),
-      resolve({
-        dedupe
-      }),
-      commonjs(),
-      json(),
-      typescript()
-    ],
-    external: Object.keys(pkg.dependencies).concat(
-      require('module').builtinModules ||
-        Object.keys(process.binding('natives'))
-    ),
+    plugins:
+        [
+          replace({
+            'process.browser' : false,
+            'process.env.NODE_ENV' : JSON.stringify(mode)
+          }),
+          svelte({preprocess, generate : 'ssr', dev}), postcss({
+            extract : './static/global.css',
+            plugins : [
+              require('postcss-import'),
+              require('tailwindcss'), // See tailwind.config.js
+              require('autoprefixer'), require('postcss-fail-on-warn'),
+              // Do not purge the CSS in dev mode to be able to play with
+              // classes in the browser dev-tools.
+              !dev && purgecss, !dev && require('cssnano')({preset : 'default'})
+            ].filter(Boolean)
+          }),
+          resolve({dedupe}), commonjs(), json(), typescript()
+        ],
+    external: Object.keys(pkg.dependencies)
+                .concat(require('module').builtinModules ||
+                        Object.keys(process.binding('natives'))),
     onwarn
   },
 
   serviceworker: {
     input: config.serviceworker.input(),
     output: config.serviceworker.output(),
-    plugins: [
-      resolve(),
-      replace({
-        'process.browser': true,
-        'process.env.NODE_ENV': JSON.stringify(mode)
-      }),
-      commonjs(),
-      !dev && terser()
-    ],
+    plugins:
+        [
+          resolve(), replace({
+            'process.browser' : true,
+            'process.env.NODE_ENV' : JSON.stringify(mode)
+          }),
+          commonjs(), !dev && terser()
+        ],
     onwarn
   }
 };
